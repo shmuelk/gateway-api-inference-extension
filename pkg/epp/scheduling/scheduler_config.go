67102731eed2278e34c4517186f8853acd75396c
Shmuel Kallner <kallner@il.ibm.com>
2025-05-28 14:18:29 +0300

    Added code to load a SchedulerConfig from the configuration text/file
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- pkg/epp/scheduling/scheduler_config.go
+++ pkg/epp/scheduling/scheduler_config.go
@@ -16,7 +16,14 @@ limitations under the License.
 
 package scheduling
 
-import "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework"
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/common/config"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework"
+)
 
 // NewSchedulerConfig creates a new SchedulerConfig object and returns its pointer.
 func NewSchedulerConfig(profilePicker framework.ProfilePicker, profiles map[string]*framework.SchedulerProfile) *SchedulerConfig {
@@ -30,4 +37,69 @@ func NewSchedulerConfig(profilePicker framework.ProfilePicker, profiles map[stri
 type SchedulerConfig struct {
 	profilePicker framework.ProfilePicker
 	profiles      map[string]*framework.SchedulerProfile
+}
+
+func LoadSchedulerConfig(theConfig *config.Config,
+	log logr.Logger) (*SchedulerConfig, error) {
+	references, err := config.LoadPluginReferences(theConfig, log)
+	if err != nil {
+		return nil, err
+	}
+
+	var profiles = map[string]*framework.SchedulerProfile{}
+
+	for _, configProfile := range theConfig.SchedulerProfiles {
+		profile := framework.SchedulerProfile{}
+
+		for _, plugin := range configProfile.Plugins {
+			var thePlugin framework.Plugin
+			var err error
+			if plugin.Reference != nil {
+				thePlugin = references[*plugin.Reference]
+			} else {
+				thePlugin, err = config.InstantiatePlugin(*plugin.Plugin, log)
+				if err != nil {
+					return nil, err
+				}
+			}
+			if theScorer, ok := thePlugin.(framework.Scorer); ok {
+				if plugin.Weight == nil {
+					var name string
+					if plugin.Reference != nil {
+						name = *plugin.Reference
+					} else {
+						name = plugin.Plugin.Plugin
+					}
+					err = fmt.Errorf("scorer %s is missing a weight", name)
+					log.Error(err, "failed to instantiate scheduler profile")
+					return nil, err
+				}
+				thePlugin = framework.NewWeightedScorer(theScorer, *plugin.Weight)
+			}
+			err = profile.AddPlugins(thePlugin)
+			fmt.Printf("\n\n==> %s \n\n", err)
+			if err != nil {
+				return nil, err
+			}
+		}
+		profiles[configProfile.Name] = &profile
+	}
+
+	var thePlugin framework.Plugin
+	var pluginName string
+
+	if theConfig.ProfilePicker.Reference != nil {
+		pluginName = *theConfig.ProfilePicker.Reference
+		thePlugin = references[pluginName]
+	} else {
+		pluginName = theConfig.ProfilePicker.Plugin.Plugin
+		thePlugin, err = config.InstantiatePlugin(*theConfig.ProfilePicker.Plugin, log)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if profilePicker, ok := thePlugin.(framework.ProfilePicker); ok {
+		return NewSchedulerConfig(profilePicker, profiles), nil
+	}
+	return nil, fmt.Errorf("the plugin %s is not a ProfilePicker", pluginName)
 }
---
+++ pkg/epp/scheduling/scheduler_config_test.go
@@ -0,0 +1,210 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scheduling
+
+import (
+	"fmt"
+	"testing"
+
+	commonconfig "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/common/config"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework/plugins/filter"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework/plugins/multi/prefix"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework/plugins/picker"
+	profilepicker "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework/plugins/profile-picker"
+	logutil "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/logging"
+)
+
+// Cause the various init() functions to registers the plugins
+var _ = profilepicker.AllProfilesPicker{}
+var _ = filter.LeastQueueFilter{}
+var _ = prefix.Plugin{}
+var _ = picker.MaxScorePicker{}
+
+func TestLoadSchedulerConfig(t *testing.T) {
+	log := logutil.NewTestLogger()
+
+	tests := []struct {
+		name       string
+		configText string
+		wantErr    bool
+	}{
+		{
+			name:       "success",
+			configText: successConfigText,
+			wantErr:    false,
+		},
+		{
+			name:       "errorBadPluginJson",
+			configText: errorBadPluginJsonText,
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadPluginNoWeight",
+			configText: errorBadPluginNoWeightText,
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadReferenceNoWeight",
+			configText: errorBadReferenceNoWeightText,
+			wantErr:    true,
+		},
+		{
+			name:       "errorPluginReferenceJson",
+			configText: errorPluginReferenceJsonText,
+			wantErr:    true,
+		},
+		{
+			name:       "errorTwoPickers",
+			configText: errorTwoPickersText,
+			wantErr:    true,
+		},
+		{
+			name:       "errorConfig",
+			configText: errorConfigText,
+			wantErr:    true,
+		},
+	}
+
+	for _, test := range tests {
+		fmt.Printf("\n\n%s\n\n", test.name)
+		theConfig, err := commonconfig.LoadConfig([]byte(test.configText), "", log)
+		if err != nil {
+			if test.wantErr {
+				continue
+			}
+			t.Fatalf("LoadConfig returned unexpected error: %v", err)
+		}
+
+		_, err = LoadSchedulerConfig(theConfig, log)
+		if err != nil {
+			if !test.wantErr {
+				t.Errorf("LoadSchedulerConfig returned an unexpected error. error %v", err)
+			}
+		} else if test.wantErr {
+			t.Errorf("LoadSchedulerConfig did not return an expected error (%s)", test.name)
+		}
+	}
+}
+
+// The following multi-line string constants, cause false positive lint errors (dupword)
+
+//nolint:dupword
+const successConfigText = `
+plugin_definitions:
+- name: lowQueue
+  plugin: low-queue
+  parameters:
+    threshold: 10
+profile_picker:
+  plugin:
+    plugin: all-profiles
+scheduler_profiles:
+- name: default
+  plugins:
+  - reference: lowQueue
+  - plugin:
+      plugin: prefix-cache
+      parameters:
+        hash-block-size: 32
+    weight: 50
+  - plugin:
+      plugin: max-score
+`
+
+//nolint:dupword
+const errorBadPluginJsonText = `
+profile_picker:
+  plugin:
+    plugin: all-profiles
+scheduler_profiles:
+- name: default
+  plugins:
+  - plugin:
+      plugin: prefix-cache
+      parameters:
+        hash-block-size: asdf
+    weight: 50
+`
+
+//nolint:dupword
+const errorBadPluginNoWeightText = `
+profile_picker:
+  plugin:
+    plugin: all-profiles
+scheduler_profiles:
+- name: default
+  plugins:
+  - plugin:
+      plugin: prefix-cache
+      parameters:
+        hash-block-size: 32
+`
+
+//nolint:dupword
+const errorBadReferenceNoWeightText = `
+profile_picker:
+  plugin:
+    plugin: all-profiles
+plugin_definitions:
+- name: prefix
+  plugin: prefix-cache
+  parameters:
+    hash-block-size: 32
+scheduler_profiles:
+- name: default
+  plugins:
+  - reference: prefix
+`
+
+//nolint:dupword
+const errorPluginReferenceJsonText = `
+plugin_definitions:
+- name: lowQueue
+  plugin: low-queue
+  parameters:
+    threshold: qwer
+profile_picker:
+  plugin:
+    plugin: all-profiles
+scheduler_profiles:
+- name: default
+  plugins:
+  - reference: lowQueue
+`
+
+//nolint:dupword
+const errorTwoPickersText = `
+profile_picker:
+  plugin:
+    plugin: all-profiles
+scheduler_profiles:
+- name: default
+  plugins:
+  - plugin:
+      plugin: max-score
+  - plugin:
+      plugin: random
+`
+
+//nolint:dupword
+const errorConfigText = `
+plugin_definitions:
+- name: lowQueue
+  plugin: low-queue
+  parameters:
+    threshold: 10
+`
