a0d673b2d64a3e9b3bb95f26008372ebec418c68
Shmuel Kallner <kallner@il.ibm.com>
2025-05-28 14:13:10 +0300

    Added confguration struct and code to load and verify it
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

---
+++ pkg/epp/common/config/configloader.go
@@ -0,0 +1,166 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework"
+	"sigs.k8s.io/yaml"
+)
+
+type Config struct {
+	PluginDefinitions []ConfigPluginDefinition `json:"plugin_definitions"`
+	ProfilePicker     BaseConfigProfilePlugin  `json:"profile_picker"`
+	SchedulerProfiles []ConfigSchedulerProfile `json:"scheduler_profiles"`
+}
+
+type BaseConfigPluginDefinition struct {
+	Plugin     string          `json:"plugin"`
+	Parameters json.RawMessage `json:"parameters"`
+}
+
+type ConfigPluginDefinition struct {
+	BaseConfigPluginDefinition `json:",inline"`
+	Name                       string `json:"name"`
+}
+
+type ConfigSchedulerProfile struct {
+	Name    string                `json:"name"`
+	Plugins []ConfigProfilePlugin `json:"plugins"`
+}
+
+type BaseConfigProfilePlugin struct {
+	Reference *string                     `json:"reference"`
+	Plugin    *BaseConfigPluginDefinition `json:"plugin"`
+}
+
+type ConfigProfilePlugin struct {
+	BaseConfigProfilePlugin `json:",inline"`
+	Weight                  *int `json:"weight"`
+}
+
+// Load config either from supplied text or from a file
+func LoadConfig(configText []byte, fileName string, log logr.Logger) (*Config, error) {
+	var err error
+	if len(configText) == 0 {
+		configText, err = os.ReadFile(fileName)
+		if err != nil {
+			log.Error(err, "failed to load config file")
+			return nil, err
+		}
+	}
+
+	theConfig := &Config{}
+	err = yaml.Unmarshal(configText, theConfig)
+	if err != nil {
+		log.Error(err, "failed to parse the configuration")
+		return nil, err
+	}
+
+	// Validate loaded configuration
+	err = validateConfiguration(theConfig)
+	if err != nil {
+		log.Error(err, "the configuration is invalid")
+		return nil, err
+	}
+	return theConfig, nil
+}
+
+func LoadPluginReferences(theConfig *Config, log logr.Logger) (map[string]framework.Plugin, error) {
+	references := map[string]framework.Plugin{}
+	for _, pluginReference := range theConfig.PluginDefinitions {
+		thePlugin, err := InstantiatePlugin(pluginReference.BaseConfigPluginDefinition, log)
+		if err != nil {
+			return nil, err
+		}
+		references[pluginReference.Name] = thePlugin
+	}
+	return references, nil
+}
+
+func InstantiatePlugin(pluginDefinition BaseConfigPluginDefinition, log logr.Logger) (framework.Plugin, error) {
+	factory, ok := framework.Registry[pluginDefinition.Plugin]
+	if !ok {
+		err := fmt.Errorf("plugin %s not found", pluginDefinition.Plugin)
+		log.Error(err, "failed to instantiate plugin")
+		return nil, err
+	}
+	thePlugin, err := factory(pluginDefinition.Parameters)
+	if err != nil {
+		log.Error(err, "failed to instantiate the plugin", "plugin", pluginDefinition.Plugin)
+		return nil, err
+	}
+	return thePlugin, err
+}
+
+func validateConfiguration(theConfig *Config) error {
+	for _, pluginDefinition := range theConfig.PluginDefinitions {
+		if pluginDefinition.Name == "" || pluginDefinition.Plugin == "" {
+			return errors.New("plugin reference definition missing name or plugin reference")
+		}
+		_, ok := framework.Registry[pluginDefinition.Plugin]
+		if !ok {
+			return fmt.Errorf("plugin %s is not found", pluginDefinition.Plugin)
+		}
+	}
+
+	if len(theConfig.SchedulerProfiles) == 0 {
+		return errors.New("there must be at least one scheduling profile in the configuration")
+	}
+
+	if theConfig.ProfilePicker.Reference == nil && theConfig.ProfilePicker.Plugin == nil {
+		return errors.New("ProfilePicker needs either a plugin reference or definition")
+	}
+
+	for _, profile := range theConfig.SchedulerProfiles {
+		if profile.Name == "" {
+			return errors.New("SchedulerProfiles need a name")
+		}
+		if len(profile.Plugins) == 0 {
+			return errors.New("SchedulingProfiles need at leas one plugin")
+		}
+		for _, plugin := range profile.Plugins {
+			if plugin.Reference == nil && plugin.Plugin == nil {
+				return errors.New("SchedulingProfile's plugins need either a plugin reference or definition")
+			}
+			if plugin.Reference != nil {
+				notFound := true
+				for _, reference := range theConfig.PluginDefinitions {
+					if *plugin.Reference == reference.Name {
+						notFound = false
+						break
+					}
+				}
+				if notFound {
+					return errors.New(*plugin.Reference + " is a reference to an undefined PluginDefinition")
+				}
+			} else {
+				_, ok := framework.Registry[plugin.Plugin.Plugin]
+				if !ok {
+					return fmt.Errorf("plugin %s is not found", plugin.Plugin.Plugin)
+				}
+			}
+		}
+
+	}
+	return nil
+}
---
+++ pkg/epp/common/config/configloader_test.go
@@ -0,0 +1,450 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/framework"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/types"
+	logutil "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/logging"
+)
+
+const (
+	testProfilePicker = "test-profile-picker"
+	test1Name         = "test-one"
+	test2Name         = "test-two"
+	testPickerName    = "test-picker"
+)
+
+func TestLoadConfiguration(t *testing.T) {
+	test1 := "test1"
+	test2Weight := 50
+
+	registerTestPlugins()
+
+	goodConfig := &Config{
+		PluginDefinitions: []ConfigPluginDefinition{
+			{
+				Name: "test1",
+				BaseConfigPluginDefinition: BaseConfigPluginDefinition{
+					Plugin:     test1Name,
+					Parameters: json.RawMessage("{\"threshold\":10}"),
+				},
+			},
+		},
+		ProfilePicker: BaseConfigProfilePlugin{
+			Plugin: &BaseConfigPluginDefinition{
+				Plugin: testProfilePicker,
+			},
+		},
+		SchedulerProfiles: []ConfigSchedulerProfile{
+			{
+				Name: "default",
+				Plugins: []ConfigProfilePlugin{
+					{
+						BaseConfigProfilePlugin: BaseConfigProfilePlugin{
+							Reference: &test1,
+						},
+					},
+					{
+						BaseConfigProfilePlugin: BaseConfigProfilePlugin{
+							Plugin: &BaseConfigPluginDefinition{
+								Plugin:     test2Name,
+								Parameters: json.RawMessage("{\"hash-block-size\":32}"),
+							},
+						},
+						Weight: &test2Weight,
+					},
+					{
+						BaseConfigProfilePlugin: BaseConfigProfilePlugin{
+							Plugin: &BaseConfigPluginDefinition{
+								Plugin: testPickerName,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		name       string
+		configText string
+		configFile string
+		want       *Config
+		wantErr    bool
+	}{
+		{
+			name:       "success",
+			configText: successConfigText,
+			configFile: "",
+			want:       goodConfig,
+			wantErr:    false,
+		},
+		{
+			name:       "errorBadYaml",
+			configText: errorBadYamlText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorNoProfilePicker",
+			configText: errorNoProfilePickerText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadPluginReferenceText",
+			configText: errorBadPluginReferenceText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadPluginReferencePluginText",
+			configText: errorBadPluginReferencePluginText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorNoProfiles",
+			configText: errorNoProfilesText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorNoProfileName",
+			configText: errorNoProfileNameText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorNoProfilePlugins",
+			configText: errorNoProfilePluginsText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadProfilePlugin",
+			configText: errorBadProfilePluginText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadProfilePluginRef",
+			configText: errorBadProfilePluginRefText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "errorBadProfilePluginName",
+			configText: errorBadProfilePluginNameText,
+			configFile: "",
+			wantErr:    true,
+		},
+		{
+			name:       "successFromFile",
+			configText: "",
+			configFile: "../../../../test/testdata/configloader_1_test.yaml",
+			want:       goodConfig,
+			wantErr:    false,
+		},
+		{
+			name:       "noSuchFile",
+			configText: "",
+			configFile: "../../../../test/testdata/configloader_error_test.yaml",
+			wantErr:    true,
+		},
+	}
+
+	log := logutil.NewTestLogger()
+	for _, test := range tests {
+		got, err := LoadConfig([]byte(test.configText), test.configFile, log)
+		if err != nil {
+			if !test.wantErr {
+				t.Fatalf("In test %s LoadConfig returned unexpected error: %v, want %v", test.name, err, test.wantErr)
+			}
+		} else {
+			if test.wantErr {
+				t.Fatalf("In test %s LoadConfig did not return an expected error", test.name)
+			}
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("In test %s LoadConfig returned unexpected response, diff(-want, +got): %v", test.name, diff)
+			}
+		}
+	}
+}
+
+func TestLoadPluginReferences(t *testing.T) {
+	log := logutil.NewTestLogger()
+
+	theConfig, err := LoadConfig([]byte(successConfigText), "", log)
+	if err != nil {
+		t.Fatalf("LoadConfig returned unexpected error: %v", err)
+	}
+	references, err := LoadPluginReferences(theConfig, log)
+	if err != nil {
+		t.Fatalf("LoadPluginReferences returned unexpected error: %v", err)
+	}
+	if len(references) == 0 {
+		t.Fatalf("LoadPluginReferences returned an empty set of references")
+	}
+	if t1, ok := references["test1"]; !ok {
+		t.Fatalf("LoadPluginReferences returned references did not contain test1")
+	} else if _, ok := t1.(*test1); !ok {
+		t.Fatalf("LoadPluginReferences returned references value for test1 has the wrong type %#v", t1)
+	}
+
+	theConfig, err = LoadConfig([]byte(errorBadPluginReferenceParametersText), "", log)
+	if err != nil {
+		t.Fatalf("LoadConfig returned unexpected error: %v", err)
+	}
+	_, err = LoadPluginReferences(theConfig, log)
+	if err == nil {
+		t.Fatalf("LoadPluginReferences did not return the expected error")
+	}
+}
+
+func TestInstantiatePlugin(t *testing.T) {
+	log := logutil.NewTestLogger()
+
+	plugReference := BaseConfigPluginDefinition{Plugin: "plover"}
+	_, err := InstantiatePlugin(plugReference, log)
+	if err == nil {
+		t.Fatalf("InstantiatePlugin did not return the expected error")
+	}
+}
+
+// The following multi-line string constants, cause false positive lint errors (dupword)
+
+//nolint:dupword
+const successConfigText = `
+plugin_definitions:
+- name: test1
+  plugin: test-one
+  parameters:
+    threshold: 10
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+scheduler_profiles:
+- name: default
+  plugins:
+  - reference: test1
+  - plugin:
+      plugin: test-two
+      parameters:
+        hash-block-size: 32
+    weight: 50
+  - plugin:
+      plugin: test-picker
+`
+
+//nolint:dupword
+const errorBadYamlText = `
+plugin_definitions:
+- testing 1 2 3
+`
+
+//nolint:dupword
+const errorBadPluginReferenceText = `
+plugin_definitions:
+- test: 1234
+`
+
+//nolint:dupword
+const errorBadPluginReferencePluginText = `
+plugin_definitions:
+- name: testx
+  plugin: test-x
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+`
+
+//nolint:dupword
+const errorNoProfilePickerText = `
+plugin_definitions:
+- name: test1
+  plugin: test-one
+  parameters:
+    threshold: 10
+scheduler_profiles:
+- name: default
+`
+
+//nolint:dupword
+const errorNoProfilesText = `
+plugin_definitions:
+- name: test1
+  plugin: test-one
+  parameters:
+    threshold: 10
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+`
+
+//nolint:dupword
+const errorNoProfileNameText = `
+plugin_definitions:
+- name: test1
+  plugin: test-one
+  parameters:
+    threshold: 10
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+scheduler_profiles:
+- test: x
+`
+
+//nolint:dupword
+const errorNoProfilePluginsText = `
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+plugin_definitions:
+- name: test1
+  plugin: test-one
+  parameters:
+    threshold: 10
+scheduler_profiles:
+- name: default
+`
+
+//nolint:dupword
+const errorBadProfilePluginText = `
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+scheduler_profiles:
+- name: default
+  plugins:
+  - name: test
+`
+
+//nolint:dupword
+const errorBadProfilePluginRefText = `
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+scheduler_profiles:
+- name: default
+  plugins:
+  - reference: plover
+`
+
+//nolint:dupword
+const errorBadProfilePluginNameText = `
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+scheduler_profiles:
+- name: default
+  plugins:
+  - plugin:
+      plugin: plover
+`
+
+//nolint:dupword
+const errorBadPluginReferenceParametersText = `
+plugin_definitions:
+- name: test1
+  plugin: test-one
+  parameters:
+    threshold: asdf
+profile_picker:
+  plugin: 
+    plugin: test-profile-picker
+scheduler_profiles:
+- name: default
+  plugins:
+  - reference: test1
+`
+
+// compile-time type validation
+var _ framework.Filter = &test1{}
+
+type test1 struct {
+	Threshold int `json:"threshold"`
+}
+
+func (f *test1) Name() string {
+	return test1Name
+}
+
+// Filter filters out pods that doesn't meet the filter criteria.
+func (f *test1) Filter(ctx *types.SchedulingContext, pods []types.Pod) []types.Pod {
+	return pods
+}
+
+// compile-time type validation
+var _ framework.PreCycle = &test2{}
+var _ framework.Scorer = &test2{}
+var _ framework.PostCycle = &test2{}
+
+type test2 struct{}
+
+func (f *test2) Name() string {
+	return test2Name
+}
+
+func (m *test2) PreCycle(ctx *types.SchedulingContext) {}
+
+func (m *test2) Score(ctx *types.SchedulingContext, pods []types.Pod) map[types.Pod]float64 {
+	return map[types.Pod]float64{}
+}
+
+func (m *test2) PostCycle(ctx *types.SchedulingContext, res *types.Result) {}
+
+// compile-time type validation
+var _ framework.Picker = &testPicker{}
+
+type testPicker struct{}
+
+func (p *testPicker) Name() string {
+	return testPickerName
+}
+
+func (p *testPicker) Pick(ctx *types.SchedulingContext, scoredPods []*types.ScoredPod) *types.Result {
+	return nil
+}
+
+func registerTestPlugins() {
+	framework.Register(test1Name,
+		func(parameters json.RawMessage) (framework.Plugin, error) {
+			result := test1{}
+			err := json.Unmarshal(parameters, &result)
+			return &result, err
+		},
+	)
+
+	framework.Register(test2Name,
+		func(parameters json.RawMessage) (framework.Plugin, error) {
+			return &test2{}, nil
+		},
+	)
+
+	framework.Register(testPickerName,
+		func(parameters json.RawMessage) (framework.Plugin, error) {
+			return &testPicker{}, nil
+		},
+	)
+}
