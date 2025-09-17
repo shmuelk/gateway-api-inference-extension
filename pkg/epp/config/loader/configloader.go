522d88f6e16ed8516a7694e71ec446ad7c8e91e0
Shmuel Kallner <kallner@il.ibm.com>
2025-09-17 21:24:55 +0300

    Moved config validation into a separate file
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- pkg/epp/config/loader/configloader.go
+++ pkg/epp/config/loader/configloader.go
@@ -187,53 +187,7 @@ func instantiatePlugins(configuredPlugins []configapi.PluginSpec, handle plugins
 	return nil
 }
 
-func validateSchedulingProfiles(config *configapi.EndpointPickerConfig) error {
-	profileNames := sets.New[string]()
-	for _, profile := range config.SchedulingProfiles {
-		if profile.Name == "" {
-			return errors.New("SchedulingProfile must have a name")
-		}
-
-		if profileNames.Has(profile.Name) {
-			return fmt.Errorf("the name '%s' has been specified for more than one SchedulingProfile", profile.Name)
-		}
-		profileNames.Insert(profile.Name)
-
-		for _, plugin := range profile.Plugins {
-			if len(plugin.PluginRef) == 0 {
-				return fmt.Errorf("SchedulingProfile '%s' plugins must have a plugin reference", profile.Name)
-			}
-
-			notFound := true
-			for _, pluginConfig := range config.Plugins {
-				if plugin.PluginRef == pluginConfig.Name {
-					notFound = false
-					break
-				}
-			}
-			if notFound {
-				return errors.New(plugin.PluginRef + " is a reference to an undefined Plugin")
-			}
-		}
-	}
-	return nil
-}
-
 // RegisterFeatureGate registers feature gate keys for validation
 func RegisterFeatureGate(gate string) {
 	registeredFeatureGates[gate] = struct{}{}
-}
-
-func validateFeatureGates(fg configapi.FeatureGates) error {
-	if fg == nil {
-		return nil
-	}
-
-	for gate := range fg {
-		if _, ok := registeredFeatureGates[gate]; !ok {
-			return errors.New(gate + " is an unregistered Feature Gate")
-		}
-	}
-
-	return nil
 }
