55778f6b74390cdb1652da04804cf5946b73d4a7
Shmuel Kallner <kallner@il.ibm.com>
2026-01-20 12:15:16 +0200

    Added the ability for Runner helpers to have plugins
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- cmd/epp/runner/runner.go
+++ cmd/epp/runner/runner.go
@@ -100,6 +100,7 @@ type RunnerHelper interface {
 		gknn common.GKNN, director *requestcontrol.Director, saturationDetector *utilizationdetector.Detector,
 		useExperimentalDatalayerV2 bool, mgr ctrl.Manager, logger logr.Logger) error
 	RegisterHealthServer(mgr manager.Manager, logger logr.Logger, ds datastore.Datastore, port int, isLeader *atomic.Bool, leaderElectionEnabled bool) error
+	AddPlugins(plugins ...plugins.Plugin)
 }
 
 // NewRunner initializes a new EPP Runner and returns its pointer.
@@ -452,6 +453,8 @@ func (r *Runner) parseConfigurationPhaseTwo(ctx context.Context, rawConfig *conf
 
 	// Handler deprecated configuration options
 	r.deprecatedConfigurationHelper(cfg)
+
+	r.helper.AddPlugins(handle.GetAllPlugins()...)
 
 	logger.Info("loaded configuration from file/text successfully")
 	return cfg, nil
