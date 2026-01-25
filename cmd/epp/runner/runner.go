f5335aa8ea8b8367e95702a0812c3ae4cc7f1315
Shmuel Kallner <kallner@il.ibm.com>
2026-01-25 15:58:46 +0200

    Changes due to refactoring in GIE
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- cmd/epp/runner/runner.go
+++ cmd/epp/runner/runner.go
@@ -59,6 +59,7 @@ import (
 	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/flowcontrol/framework/plugins/ordering"
 	fcregistry "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/flowcontrol/registry"
 	fwkdl "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/interface/datalayer"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/interface/plugin"
 	fwkplugin "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/interface/plugin"
 	extractormetrics "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/plugins/datalayer/extractor/metrics"
 	sourcemetrics "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/plugins/datalayer/source/metrics"
@@ -106,7 +107,7 @@ type RunnerHelper interface {
 		gknn common.GKNN, director *requestcontrol.Director, saturationDetector *utilizationdetector.Detector,
 		useExperimentalDatalayerV2 bool, mgr ctrl.Manager, logger logr.Logger) error
 	RegisterHealthServer(mgr manager.Manager, logger logr.Logger, ds datastore.Datastore, port int, isLeader *atomic.Bool, leaderElectionEnabled bool) error
-	AddPlugins(plugins ...plugins.Plugin)
+	AddPlugins(plugins ...plugin.Plugin)
 }
 
 // NewRunner initializes a new EPP Runner and returns its pointer.
